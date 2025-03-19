1c738f4ee4ee8aeea0fcd9009ba2fac5e248d653
rogervfaria <fariiaroger@gmail.com>
2025-03-19 16:53:43 -0300

    Implementação de melhorias e otimização do código.

--- main.py
+++ main.py
@@ -18,31 +18,42 @@ extrato = ''
 
 while True:
     print(menu)
-    opcao = int(input('Digite a opção desejada: '))
+    opcao = int(input('\nDigite a opção desejada: '))
 
     if opcao == 2:
-        saque = float(input('Valor do saque: R$ '))
+        saque = float(input('\nValor do saque: R$ '))
 
         if saque > limite_saque:
-            print('ERRO! O valor do saque excede o limite de R$ 500,00.')
+            print('\nERRO! O valor do saque excede o limite de R$ 500,00.')
         
         elif saque > saldo_conta:
-            print('Não é possível realizar o saque por falta de saldo.')
+            print('\nNão é possível realizar o saque por falta de saldo.')
 
         elif contador_saque > limite_saque_diario:
             print('ERRO! Você excedeu o limite de saques de 3 vezes/dia.')
 
+        elif contador_saque >= limite_saque_diario:
+            print('\nERRO! Você excedeu o limite de saques de 3 vezes/dia.')
+
         else:
-            saldo_conta -= saque
-            contador_saque += 1
-            print(f'Saque de R$ {saque:.2f} realizado com sucesso!')
-            extrato += f'Saque: R$ {saque:.2f}\n'
+            if saque > 0:
+                saldo_conta -= saque
+                contador_saque += 1
+                print(f'\nSaque de R$ {saque:.2f} realizado com sucesso!')
+                extrato += f'Saque: R$ {saque:.2f}\n'
+            else:
+                print('\nERRO! Valor inválido.')
             
     elif opcao == 1:
-        deposito = float(input('Valor do depósito: R$ '))
-        saldo_conta += deposito
-        extrato += f'Depósito: R$ {deposito:.2f}\n'
-        print(f'Depósito de R$ {deposito:.2f} realizado com sucesso!')
+        deposito = float(input('\nValor do depósito: R$ '))
+
+        if deposito > 0:
+            saldo_conta += deposito
+            extrato += f'Depósito: R$ {deposito:.2f}\n'
+            print(f'\nDepósito de R$ {deposito:.2f} realizado com sucesso!')
+
+        else:
+            print('\nERRO! Valor inválido.')
     
     elif opcao == 3:
 
@@ -56,8 +67,13 @@ while True:
             print(f'{extrato}\nSaldo da conta: R$ {saldo_conta:.2f}')
             print('=' * 30)
         
+            print('====================EXTRATO====================\n')
+            print('Não foram realizadas movimentações nesta conta!\n'if not extrato else extrato)
+            print(f'Saldo da conta: R$ {saldo_conta:.2f}\n')
+            print('=' * 47)
+        
     elif opcao == 4:
         break
 
     else:
-        print('Opção inválida, por favor selecione novamente a operação desejada.')
+        print('\nOpção inválida, por favor selecione novamente a operação desejada.')
