f87ca086ef179b67618e4e499b7323e37813cbd6
rogervfaria <fariiaroger@gmail.com>
2025-03-21 17:36:49 -0300

    Adiciona as operações em funções e implementação de data e hora das transações.

--- main.py
+++ main.py
@@ -1,70 +1,101 @@
 from datetime import datetime
 
-menu = '''
-------------------
-       MENU
-------------------
-[1] -> Depositar
-[2] -> Sacar
-[3] -> Extrato
-[4] -> Sair
-'''
 
-limite_saque = 500
-limite_trasacoes_diario = 10
-contador_transacoes = 0
+def menu():
+    menu = '''
+    ------------------
+           MENU
+    ------------------
+    [1] -> Depositar
+    [2] -> Sacar
+    [3] -> Extrato
+    [4] -> Sair
+    '''
 
-saldo_conta = 0
-extrato = ''
+    return input(f'{menu}\nDigite a opção desejada: ')
 
-while True:
-    print(menu)
-    opcao = int(input('Digite a opção desejada: '))
 
-    if opcao == 2:
-        saque = float(input('\nValor do saque: R$ '))
+def depositar(valor, saldo, extrato, contador_transacoes, limite_transacoes, /):
+    if contador_transacoes >= limite_transacoes:
+        print('\nERRO! Você excedeu o limite de 10 transações por dia.')
 
-        if saque > limite_saque:
-            print('\nERRO! O valor do saque excede o limite de R$ 500,00.')
-        
-        elif saque > saldo_conta:
-            print('\nNão é possível realizar o saque por falta de saldo.')
-
-        elif contador_transacoes >= limite_trasacoes_diario:
-            print('\nERRO! Você excedeu o limite de 10 transações por dia.')
-
-        else:
-            if saque > 0:
-                saldo_conta -= saque
-                contador_transacoes += 1
-                print(f'\nSaque de R$ {saque:.2f} realizado com sucesso!')
-                extrato += f'Saque: R$ {saque:.2f} | {datetime.now()}\n'
-            else:
-                print('\nERRO! Valor inválido.')
-            
-    elif opcao == 1:
-        deposito = float(input('\nValor do depósito: R$ '))
-
-        if contador_transacoes >= limite_trasacoes_diario:
-            print('\nERRO! Você excedeu o limite de 10 transações por dia.')
-
-        elif deposito < 0:
-            print('\nERRO! Valor inválido.')
-
-        else: 
-            saldo_conta += deposito
-            contador_transacoes += 1
-            extrato += f'Depósito: R$ {deposito:.2f} | {datetime.now()}\n'
-            print(f'\nDepósito de R$ {deposito:.2f} realizado com sucesso!')
-
-    elif opcao == 3:
-            print('=========================EXTRATO=========================\n')
-            print('Não foram realizadas movimentações nesta conta!\n' if not extrato else extrato)
-            print(f'Saldo da conta: R$ {saldo_conta:.2f}\n')
-            print('=' * 57)
-        
-    elif opcao == 4:
-        break
+    elif valor > 0:
+        saldo += valor
+        extrato += f'Depósito: R$ {valor:.2f} | {datetime.now()}\n'
+        contador_transacoes += 1
+        print(f'\nDepósito de R$ {valor:.2f} realizado com sucesso!')
 
     else:
-        print('\nOpção inválida, por favor selecione novamente a operação desejada.')
+        print('\nERRO! Valor inválido.')
+
+    return saldo, extrato, contador_transacoes
+    
+
+def sacar(*, valor, saldo, extrato, limite_saque, limite_transacoes, contador_transacoes):
+    if valor > limite_saque:
+        print('\nERRO! O valor do saque excede o limite de R$ 500,00.')
+
+    elif contador_transacoes >= limite_transacoes:
+        print('\nERRO! Você excedeu o limite de 10 transações por dia.')
+
+    elif valor > saldo:
+        print('\nNão é possível realizar o saque por falta de saldo.')
+
+    else:
+        saldo -= valor
+        extrato += f'Saque: R$ {valor:.2f} | {datetime.now()}\n'
+        contador_transacoes += 1
+        print(f'\nSaque de R$ {valor:.2f} realizado com sucesso!')
+
+    return saldo, extrato, contador_transacoes
+
+
+def exibir_extrato(saldo, /, *, extrato):
+    print('\n=========================EXTRATO=========================\n')
+    print('Não foram realizadas movimentações nesta conta!\n' if not extrato else extrato)
+    print(f'Saldo da conta: R$ {saldo:.2f}\n')
+    print('=' * 57)
+
+
+def main():
+    limite_saque = 500
+    limite_transacoes = 10
+    contador_trasacoes = 0
+    saldo = 0
+    extrato = ''
+
+    while True:
+        opcao = int(menu())
+
+        if opcao == 1:
+            valor = float(input('Valor do depósito: R$ '))
+            saldo, extrato, contador_trasacoes = depositar(
+                valor, 
+                saldo, 
+                extrato, 
+                contador_trasacoes, 
+                limite_transacoes
+            )
+
+        elif opcao == 2:
+            valor = float(input('Valor do saque: R$ '))
+            saldo, extrato, contador_trasacoes = sacar(
+                valor=valor, 
+                saldo=saldo,
+                extrato=extrato,
+                limite_saque=limite_saque,
+                limite_transacoes=limite_transacoes,
+                contador_transacoes=contador_trasacoes
+            )
+
+        elif opcao == 3:
+            exibir_extrato(saldo, extrato=extrato)
+
+        elif opcao == 4:
+            break
+        
+        else:
+            print('Opção inválida! Tente novamente.')
+
+
+main()
